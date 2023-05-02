caa64b87f3933f4fe69c6fc99a09ce2069c380af
Douglas Mendes <dmendescruz@gmail.com>
2023-05-02 20:20:53 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  // if(html.classList.contains('light')){
+  //   html.classList.remove('light')
+  // } else {
+  //   html.classList.add('light')
+  // }
+
+  html.classList.toggle('light')
+
+  const image = document.querySelector("#profile img")
+
+  if(html.classList.contains('light')) {
+    image.setAttribute('src', './assets/avatar-light.JPG')
+  } else {
+    image.setAttribute("src", "./assets/avatar.JPG")
+  }
+}
